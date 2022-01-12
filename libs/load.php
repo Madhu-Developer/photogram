bdab5a4858d9ab6dfc1a66fcb65afb232535c964
madhu  saravanana <madhusaravanan9150@gmail.com>
2022-01-12 18:05:14 +0000

    photo

---
+++ libs/load.php
@@ -0,0 +1,6 @@
+<?php
+function load_Template($name){
+    //  print(" includeing ". __DIR__."/../_templates/$name.php ");   *path* 
+    include __DIR__."/../_templates/$name.php";  
+}
+?>
---
+++ test.php
@@ -0,0 +1,8 @@
+<pre>
+    <?
+   // print_r($_SERVER);
+   // print_r($GLOBALS);
+    print_r(  __LINE__  );
+    
+    ?>
+</pre>
